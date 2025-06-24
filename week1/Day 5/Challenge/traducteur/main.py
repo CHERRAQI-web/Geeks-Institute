1d63777509582b2890873a2f1f7287d0d61e0a0c
CHERRAQI <aziza.cherraqi@gmail.com>
2025-06-24 23:25:33 +0100

    Traducteur

--- week1/Day 5/Challenge/traducteur/main.py
+++ week1/Day 5/Challenge/traducteur/main.py
@@ -1,10 +1,27 @@
 from translate import Translator
 
-translator = Translator(from_lang="en", to_lang="fr")
+# # Create a translator object from English to French
+# translator = Translator(from_lang="en", to_lang="fr")
 
-text = input("Enter you text: ")
+# # Text to translate
+# text = "Hello, how are you?"
 
-# Translate the text
-translation = translator.translate(text)
+# # Translate the text
+# translation = translator.translate(text)
 
-print(translation)  # Output: Bonjour, comment êtes-vous ?
+# print(translation)  # Output: Bonjour, comment êtes-vous ?
+
+french_words=[]
+donner=str(input("Donner une liste : "))
+french_words.append(donner)
+trad=[]
+translator = Translator(from_lang="fr", to_lang="en")
+french_words = [mot.strip() for mot in donner.split(",")]
+
+for word in french_words:
+    
+        translation = translator.translate(word)
+        trad.append(translation)
+dictionnaire = dict(zip(french_words, trad))
+
+print(dictionnaire)
