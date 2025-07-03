fc6a574c248840c50c0bcd0f559c82ac8522d865
CHERRAQI <aziza.cherraqi@gmail.com>
2025-07-03 17:14:46 +0100

    Exercice XP Gold

---
+++ week3/Day2/Exercice XP Gold/Exercice1.js
@@ -0,0 +1,8 @@
+let numbers = [123, 8409, 100053, 333333333, 7]
+for (let N=0;N<numbers.length;N++){
+    if(numbers[N]%3==0){
+        console.log(true)
+    }else{
+        console.log(false)
+    }
+}
